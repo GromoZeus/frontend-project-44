205c06cb25fae8d86dfdc53471bf096b75ceb19b
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-02-06 21:31:18 +0700

    add file src/cli.js, greation func getQuestion

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+export const getQuestion = (question) => {
+    const item = readlineSync.question(question).trim();
+    if (item === '') {
+        console.log('Please enter a valid value');
+        return getQuestion(question);
+    }
+    return item;
+};
