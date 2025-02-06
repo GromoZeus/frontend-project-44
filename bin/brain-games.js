18cd450be3bfd05bcc1ceed0401edb82fbc0cf4f
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-02-06 21:35:54 +0700

    mod file bin/brain-games.js->creating a greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import { getQuestion } from '../src/cli.js';
 
 console.log("Welcome to the Brain Games!");
+const name = getQuestion('May I have your name? ');
+console.log(`Hello, ${name}!`);
