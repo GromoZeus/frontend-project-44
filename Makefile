6d1abff51d7a67f721b6f600b373e01f0e43ccda
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-02-07 23:22:28 +0700

    install Eslint

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ install: # установка зависимостей
 publish: # публикация пакета
 	npm publish --dry-run
 
+lint: # проверка кода
+	npx eslint .
+
 brain-games: # старт игры
 	node bin/brain-games.js
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { getQuestion } from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 const name = getQuestion('May I have your name? ');
 console.log(`Hello, ${name}!`);
