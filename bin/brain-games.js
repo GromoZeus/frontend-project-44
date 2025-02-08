7fb7ed6f9c9ac8a52f6cd04babb6e6379ecb57db
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-02-08 22:29:37 +0700

    mod files src/cli.js and bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import { getQuestion } from '../src/cli.js';
+import getUser from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const name = getQuestion('May I have your name? ');
-console.log(`Hello, ${name}!`);
+getUser();
--- src/cli.js
+++ src/cli.js
@@ -8,3 +8,13 @@ export const getQuestion = (question) => {
   }
   return item;
 };
+
+export const getUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = getQuestion('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  return name;
+};
+
+export default getUser;
